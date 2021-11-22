bfb15136835fd4580b536313d33bd74c487208fb
CTemp3 <92496739+CTemp3@users.noreply.github.com>
2021-11-21 23:05:23 -0700

    JS wireframe mostly laid out, css and html likely need some tweaks but I'm going to bed so...

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// initial variable declaration
+var startbtn = document.getElementById('start-btn')
+var 
+
+// quiz start function
+var startQuiz = function() {
+
+}
+
+// question change function
+var changeQuestion = function() {
+
+}
+
+// right or wrong answer statement
+var rightOrWrong = function() {
+    /* if () {
+
+     } else {
+
+     } */
+}
+
+// timer function
+var timer = function() {
+
+}
